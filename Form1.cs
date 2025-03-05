bffbd072e80be1c4374cb58ecb074af5c978aa47
luthfi rezkiansyah ramadhani <daniramadh47@gmail.com>
2025-03-05 16:36:02 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -5,18 +5,14 @@ namespace tp_Modul3
 {
     public partial class Form1 : Form
     {
-        // Constructor untuk Form1
         public Form1()
         {
             InitializeComponent();
         }
-
-        // Event untuk Button click
         private void button1_Click(object sender, EventArgs e)
         {
             string namaPraktikan = textBox1.Text.Trim();
 
-            // Tampilkan output di Label
             if (!string.IsNullOrEmpty(namaPraktikan)) 
             {
                 labelOutput.Text = "Halo " + namaPraktikan; 
